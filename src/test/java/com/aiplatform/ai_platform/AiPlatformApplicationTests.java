ef459d11381b8dc2d0d22b293cf6550297c8614f
Majo <majose2006@gmail.com>
2026-04-17 17:26:48 -0500

    Initial commit - Spring Boot app with Dockerfile for Render

---
+++ src/test/java/com/aiplatform/ai_platform/AiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiplatform.ai_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
